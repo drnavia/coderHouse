e5b4e468381aedd778bd4933f5338a0b61d3127d
jonathan stampella <jona.stampella@gmail.com>
2021-10-13 11:58:38 -0300

    modulo_js

---
+++ modulo_javascript/clase-01/apps.js
@@ -0,0 +1,47 @@
+console.log("hola mundo");
+
+//declaramos nuestras variables
+
+//Variables
+//Objetos
+//Arrays
+//metodos
+//propiedades
+//funciones
+//clases
+
+
+//3 tipos de datos
+//string
+//var nombre = "jonathan"; // la declaracion var se remplazo con let --->
+let apellido = "stampella";
+let nombre = "jonathan";
+const nacionalidad="Argentina";
+
+
+nombre ="jonathan jesus";
+console.log(apellido);
+console.log(nombre);
+console.log(nacionalidad);
+
+//Variables number
+let numero = 18;
+
+//variables boolean
+let tengoHambre = true;
+
+
+//ingresar y egresar datos
+
+//ingresar
+let nombre_nuevo = prompt("ingresa tu nombre");
+
+console.log(nombre_nuevo);
+
+//valores en pantalla
+document.write(nombre_nuevo);
+alert("Tu nombre es:"+nombre_nuevo);
+
+let aceptar = confirm("esto es un mensaje de confirmacion");
+
+console.log(aceptar);
