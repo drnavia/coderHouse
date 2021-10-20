727bc4be26101c1704e5a0f9bc1955f9c8f6eb5f
jonathan stampella <jona.stampella@gmail.com>
2021-10-20 12:29:13 -0300

    extra

--- modulo_javascript/clase-02/app.js
+++ modulo_javascript/clase-02/app.js
@@ -1,1 +1,14 @@
 console.log("hola mundi");
+
+let edad = 15;
+
+edad++;
+console.log(edad);
+edad--;
+console.log(edad);
+
+////////////
+let base = 5;
+let altura = 10;
+let superficie = (base*altura)/2;
+console.log(superficie);
